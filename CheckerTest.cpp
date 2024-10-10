b37511bd199fc7857c73267ad81c3336fefa6541
subhashjogi15 <subhashjogi9184@gmail.com>
2024-10-10 17:05:29 +0530

    Update CheckerTest.cpp

--- CheckerTest.cpp
+++ CheckerTest.cpp
@@ -29,12 +29,12 @@ void testisBatteryOk() {
 /*	// Test case where battery is OK and display warning of Approaching discharge
     assert(isBatteryOk(40, 21, 0.7, message, warning) == true);
     assert(message == "Battery is OK.");
-	assert(warning == "Warning: Approaching discharge!");*/
+	assert(warning == "Warning: Approaching discharge!");
 
 	// Test case where battery is OK and display warning of Approaching charge-peak
     assert(isBatteryOk(40, 78, 0.7, message, warning) == true);
     assert(message == "Battery is OK.");
-	assert(warning == "Warning: Approaching charge-peak!");
+	assert(warning == "Warning: Approaching charge-peak!");*/
 
     // Test case where temperature is too low
     assert(isBatteryOk(-1, 70, 0.7, message, warning) == false);
