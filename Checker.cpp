c88cc1a67b878b70bca7390b4802ce4fe904a1d9
subhashjogi15 <subhashjogi9184@gmail.com>
2024-10-10 14:44:45 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -51,12 +51,44 @@ bool performCheck(float temperature, float soc, float chargeRate, std::string &m
 		return false;
 	}
 }
+
+void displayLowTempratureWarningIfInRange(float temperature, std::string &warning) {
+	if (temperature > 0) && (temperature < 2.25) {
+		warning = "Warning: Approaching Low temperature!";
+	}
+}
+
+void displayHighTempratureWarningIfInRange(float temperature, std::string &warning) {
+	if (temperature > 42.75) && (temperature < 45) {
+		warning = "Warning: Approaching High temperature!";
+	}
+}
+
+void displaySOCDischargeWarningIfInRange(float soc, std::string &warning) {
+	if (soc > 20) && (soc < 24) {
+		warning = warning + "and " + "Warning: Approaching discharge!";
+	}
+}
+
+void displaySOCChargePeakWarningIfInRange(float soc, std::string &warning) {
+	if (soc > 76) && (soc < 80) {
+		warning = warning + "and " + "Warning: Approaching charge-peak!";
+	}
+}
+
+displayWarningIfWithinRange(float temperature, float soc, float chargeRate, std::string &warning) {
+	displayLowTempratureWarningIfInRange(temperature, warning);
+	displayHighTempratureWarningIfInRange(temperature, warning);
+	displaySOCDischargeWarningIfInRange(soc, warning);
+	displaySOCChargePeakWarningIfInRange(soc, warning);
+}
  
-bool batteryIsOk(float temperature, float soc, float chargeRate, std::string &message) {
+bool isBatteryOk(float temperature, float soc, float chargeRate, std::string &message, std::string &warning) {
     bool allChecksOk = performCheck(temperature, soc, chargeRate, message);
  
     if (allChecksOk) {
         message = "Battery is OK.";
+	    displayWarningIfWithinRange(temperature, soc, chargeRate, warning);
 		return true;
     }
 	else {
@@ -64,36 +96,37 @@ bool batteryIsOk(float temperature, float soc, float chargeRate, std::string &me
 	}
 }
  
-void testBatteryIsOk() {
+void testisBatteryOk() {
     std::string message;
+	std::string warning;
  
     // Test case where battery is OK
-    assert(batteryIsOk(25, 70, 0.7, message) == true);
+    assert(isBatteryOk(25, 70, 0.7, message, warning) == true);
     assert(message == "Battery is OK.");
  
     // Test case where temperature is too low
-    assert(batteryIsOk(-1, 70, 0.7, message) == false);
+    assert(isBatteryOk(-1, 70, 0.7, message, warning) == false);
     assert(message == "Temperature too low!");
  
     // Test case where temperature is too high
-    assert(batteryIsOk(50, 70, 0.7, message) == false);
+    assert(isBatteryOk(50, 70, 0.7, message, warning) == false);
     assert(message == "Temperature too high!");
  
     // Test case where SOC is too low
-    assert(batteryIsOk(25, 10, 0.7, message) == false);
+    assert(isBatteryOk(25, 10, 0.7, message, warning) == false);
     assert(message == "State of Charge too low!");
  
     // Test case where SOC is too high
-    assert(batteryIsOk(25, 100, 0.7, message) == false);
+    assert(isBatteryOk(25, 100, 0.7, message, warning) == false);
     assert(message == "State of Charge too high!");
  
     // Test case where charge rate is too high
-    assert(batteryIsOk(25, 70, 0.9, message) == false);
+    assert(isBatteryOk(25, 70, 0.9, message, warning) == false);
     assert(message == "Charge Rate too high!");
 }
  
 int main() {
-    testBatteryIsOk();
+    testisBatteryOk();
     cout << "All tests passed.\n";
     return 0;
 }
